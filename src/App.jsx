c11595bd1015f9e084038a7426884c7038d16dfe
johnmarco123 <johnmarcotasillo@gmail.com>
2023-10-01 17:16:50 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
+
+function App() {
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <SearchParams />
+        </div>
+    )
+};
+
+
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
